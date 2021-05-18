0c8c9e7915f5a89f512018b08b07a8f3a51179f6
devinepk <devine.pk@gmail.com>
2021-05-18 06:53:24 -0400

    Adds appliance view

---
+++ Views/Contracts/Appliances.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WinningOffer.Data;
+using WinningOffer.Models;
+
+namespace WinningOffer.Views.Contracts
+{
+    public class AppliancesModel : PageModel
+    {
+        private readonly WinningOffer.Data.WinningOfferContext _context;
+
+        public AppliancesModel(WinningOffer.Data.WinningOfferContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Contract Contract { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Contract.Add(Contract);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
