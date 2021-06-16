19521a81fff18ca0321a129d1bd9155de91b100b
devinepk <devine.pk@gmail.com>
2021-06-15 22:54:57 -0400

    Adds class for sendGrid, adds SendGrid nuget package.

---
+++ Models/AuthMessageSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightningOffer.Models
+{
+    public class AuthMessageSender
+    {
+        public string SendGridUser { get; set; }
+        public string SendGridKey { get; set; }
+    }
+}
