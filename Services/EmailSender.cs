572c679dd28702c388917310a0011a392d1d7e9f
devinepk <devine.pk@gmail.com>
2021-06-16 12:03:20 -0400

    Fixes email address the confirmation is sent from.

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -26,7 +26,7 @@ namespace LightningOffer.Services
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
-                From = new EmailAddress("Joe@contoso.com", Options.SendGridUser),
+                From = new EmailAddress("Philip@Devineandcompany.com", Options.SendGridUser),
                 Subject = subject,
                 PlainTextContent = message,
                 HtmlContent = message
