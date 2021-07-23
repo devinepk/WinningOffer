1d52554441282aaec79a10b98482d7e69bf72dec
devinepk <devine.pk@gmail.com>
2021-07-23 09:32:46 -0400

    Hardcodes apr value in view, adds preliminary logic

--- Controllers/FinancialsController.cs
+++ Controllers/FinancialsController.cs
@@ -67,12 +67,14 @@ namespace LightningOffer.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Guid Id, [Bind("Financial_id,CreatedDate,Purchase_Price,EMD,Cash,LineOfEquity,Gift,Other_Financing,Conventional,FHA,VA,Fixed_Rate,Loan_Length,Interest_Rate,ARM_Limits,Buyer_Loan_Application_Start,EMD_With_ListingBroker,EMD_With_SellingBroker")] Financial financial)
+        public async Task<IActionResult> Create(int ARM_Limits, string Buyer_Loan_Application_Start, int Interest_Rate, int Loan_Length, string RateType, string LoanType, string DownPaymentFormat, Guid Id, int PurchasePrice, string EMD, bool EMD_With_ListingBroker, bool EMD_With_SellingBroker, string DownPaymentSource, string DownPaymentAmount,[Bind("Financial_id,CreatedDate,Purchase_Price,EMD,Cash,LineOfEquity,Gift,Other_Financing,Conventional,FHA,VA,Fixed_Rate,Loan_Length,Interest_Rate,ARM_Limits,Buyer_Loan_Application_Start,EMD_With_ListingBroker,EMD_With_SellingBroker")] Financial financial)
         {
             // Assign GUIDs and userID
+            // TODO: Purchase price, EMD with listing broker/selling broker, save
             
             Financial newFinancial = new();
             newFinancial.Financial_id = Guid.NewGuid();
+            newFinancial.CreatedDate = DateTime.Now;
 
             Guid contractId = Id; // pass from appliances
             newFinancial.ContractId = Id;
@@ -84,6 +86,52 @@ namespace LightningOffer.Controllers
             _logger.LogInformation("New financial section with ID {newFinancial.Financial_id} created with contract ID {contractId} by User {userId}", newFinancial.Financial_id, contractId, userId);
 
             // Financial specific logic
+            newFinancial.Purchase_Price = PurchasePrice;
+            newFinancial.EMD = EMD;
+            
+            if (EMD_With_ListingBroker == true) // if EMD is with listing broker...
+            { 
+                newFinancial.EMD_With_ListingBroker = EMD_With_ListingBroker; // set listing broker to true
+                newFinancial.EMD_With_SellingBroker = false; // and selling broker to false
+            } else if (EMD_With_ListingBroker == false) // otherwise, if the listng broker is false...
+            {
+                newFinancial.EMD_With_ListingBroker = false; // set it to false
+                newFinancial.EMD_With_SellingBroker = true; // and set the selling broker to true
+            }
+
+            newFinancial.DownPaymentSource = DownPaymentSource;
+            newFinancial.DownPaymentAmount = DownPaymentAmount;
+
+            string DownPaymentPercent = newFinancial.DownPaymentAmount;
+            string DownpaymentDollar = newFinancial.DownPaymentAmount;
+
+            // Append or prepend the down payment method to the down payment amount
+            if (DownPaymentFormat == "%")
+            {
+
+                newFinancial.DownPaymentAmount = DownPaymentPercent + DownPaymentFormat;
+
+            } else if (DownPaymentFormat == "$")
+            {
+
+                newFinancial.DownPaymentAmount = DownPaymentFormat + DownpaymentDollar;
+
+            }
+
+            // Financing
+            newFinancial.LoanType = LoanType;
+            newFinancial.Rate_Type = RateType;
+            newFinancial.Loan_Length = Loan_Length;
+
+            newFinancial.Interest_Rate = Interest_Rate;
+
+            //TODO: API Call for current rate
+
+            newFinancial.ARM_Limits = ARM_Limits;
+            newFinancial.Buyer_Loan_Application_Start = Buyer_Loan_Application_Start;
+
+
+
 
             if (ModelState.IsValid)
             {
