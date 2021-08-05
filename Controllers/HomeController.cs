8ed4489e4b03e955104f53bd8b37ee6fb85f0c19
devinepk <devine.pk@gmail.com>
2021-08-05 07:40:20 -0400

    Updates home page, adds comments to program.cs about configs.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,5 +37,6 @@ namespace LightningOffer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
