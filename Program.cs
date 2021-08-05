c6b0b6174151804561a4cc5390aa5a3925eaf428
devinepk <devine.pk@gmail.com>
2021-08-05 11:37:06 -0400

    changes

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace LightningOffer
                             webBuilder.UseStartup<Startup>();
                         });
 #else
-
+              
                         // For pushing to production
 
                         .ConfigureAppConfiguration((context, config) =>
