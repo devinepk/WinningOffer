fa3c829f9536091f3a47480a5eafa1fb2cada891
devinepk <devine.pk@gmail.com>
2021-08-24 10:01:17 -0400

    Adds field for home warranty selection to Home warranty model

---
+++ Migrations/20210824135953_UpdatesHomeWarrantyAddsWhoPays.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LightningOffer.Migrations
+{
+    public partial class UpdatesHomeWarrantyAddsWhoPays : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "who_chooses",
+                table: "HomeWarranty",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "who_chooses",
+                table: "HomeWarranty");
+        }
+    }
+}
